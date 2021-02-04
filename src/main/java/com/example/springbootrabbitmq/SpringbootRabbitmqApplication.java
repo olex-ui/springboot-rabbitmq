14209e84d759bfa17263069066dc39d31f9c859f
olex-ui <2941176308@qq.com>
2021-02-04 11:05:46 +0800

    dev分支第一次添加

--- src/main/java/com/example/springbootrabbitmq/SpringbootRabbitmqApplication.java
+++ src/main/java/com/example/springbootrabbitmq/SpringbootRabbitmqApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootRabbitmqApplication {
 
     public static void main(String[] args) {
+        System.out.println("dev 分支");
         SpringApplication.run(SpringbootRabbitmqApplication.class, args);
     }
 
