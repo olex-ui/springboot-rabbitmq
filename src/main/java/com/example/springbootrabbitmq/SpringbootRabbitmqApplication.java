26d252e6e22f8cb88ee4481776ece59950c4a65b
olex-ui <2941176308@qq.com>
2021-02-04 11:07:26 +0800

    master主干第一次添加

--- src/main/java/com/example/springbootrabbitmq/SpringbootRabbitmqApplication.java
+++ src/main/java/com/example/springbootrabbitmq/SpringbootRabbitmqApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootRabbitmqApplication {
 
     public static void main(String[] args) {
+        System.out.println("master主干第一次添加");
         SpringApplication.run(SpringbootRabbitmqApplication.class, args);
     }
 
